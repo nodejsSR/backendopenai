7c92b95c0defba409dee9cf997f00af605f69a55
cristian <javascriptsr1506@gmail.com>
2023-07-18 11:29:50 -0500

    add hardcore token

--- app/TokenManagement.ts
+++ app/TokenManagement.ts
@@ -1,11 +1,11 @@
 import jwt from "jsonwebtoken"
 type userType = "MASTER" | "CLIENT" | "STUDENT"
 export const createToken = (data:object,type:userType) => {
-    const seed = process.env.TOKEN_SEED
+    const seed = "Y9yNYTQVki6MLnGnQORQHg"
     return jwt.sign({...data,type:type},seed as string)
 }
 
 export const verifytoken = (token:string)=>{
-    const seed = process.env.TOKEN_SEED
+    const seed = "Y9yNYTQVki6MLnGnQORQHg"
     return jwt.verify(token,seed as string)
 }
